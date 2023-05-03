507e43a8e7cd4c148ba5a8ed8b2587232e965f0b
41Uday <uday1729vv@gmail.com>
2023-05-03 12:16:05 +0530

    sass-2nd

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return(
         <center>
             <h1>This is a Home Route</h1>
-            <p>This is parapraph with style I have used sass</p>
+            <p>This is parapraph for  styling I have used sass</p>
         </center>
     )
 }
